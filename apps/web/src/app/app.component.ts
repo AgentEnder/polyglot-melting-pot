eb0c669a04ae27cfcf2b5bd4fbe6e9f0b6cca126
AgentEnder <craigorycoppola@gmail.com>
2023-01-13 16:25:51 -0500

    chore: scaffold angular app

---
+++ apps/web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'polyglot-melting-pot-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'web';
+}
